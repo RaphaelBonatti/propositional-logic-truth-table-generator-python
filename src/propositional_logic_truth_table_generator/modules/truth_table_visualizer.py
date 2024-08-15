7019a24803ac372cd49964e59c4d8c7c1f86e576
Raphael <raphael.bonatti@protonmail.com>
2024-08-15 23:49:10 +0200

    refactor: remove useless line

--- src/propositional_logic_truth_table_generator/modules/truth_table_visualizer.py
+++ src/propositional_logic_truth_table_generator/modules/truth_table_visualizer.py
@@ -6,7 +6,9 @@ from modules.truth_table_generator import TruthTable
 class TruthTableVisualizer:
     def __init__(self, truth_table: TruthTable, as_int: bool = False) -> None:
         self._table = PrettyTable(field_names=truth_table.header)
-        self._table.title = f"Truth table for wff: {self._table.field_names[-1]}"
+        self._table.title = (
+            f"Truth table for wff: {self._table.field_names[-1]}"
+        )
         self._table.align = "c"
 
         if as_int:
@@ -20,7 +22,6 @@ class TruthTableVisualizer:
         return self._table.get_string()  # type: ignore
 
     def save_as_html(self, filename: str) -> None:
-        filename = filename if filename else "table.html"
         table_html = self._table.get_html_string(format=True)  # type: ignore
         with open(filename, mode="w", encoding="utf-8") as file:
             file.write(
